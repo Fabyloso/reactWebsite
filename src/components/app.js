58910f716a7ae87bc8c39bcea38d28260438abc6
fabyloso <faby.loso@hotmail.com>
2017-07-18 07:28:14 -0700

    move resume list

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import ResumeList from '../containers/resume-list'
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <ResumeList/>
+      </div>
     );
   }
 }
