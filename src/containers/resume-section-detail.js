e6dee312baca27c0c5d7aaae7fe02bb495e8ce7a
fabyloso <faby.loso@hotmail.com>
2017-07-18 07:27:45 -0700

    add section detail

---
+++ src/containers/resume-section-detail.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+class SectionDetail extends Component{
+    render(){
+        if(!this.props.section){
+        return;
+        }
+        return (
+            <div>
+                <h3>Details for:</h3>
+                <div>Title: {this.props.section}</div>
+            </div>
+        );
+    }
+}
+
+
+function mapStateToProps(state) {
+    return {
+        section: state.activeSection
+    };
+}
+
+export default connect(mapStateToProps)(SectionDetail);
